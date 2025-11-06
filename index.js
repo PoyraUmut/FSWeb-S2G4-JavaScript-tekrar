da21fa3e9a4435fad2f213315407483c2070d22f
Umut Tekin <tekinumut9@gmail.com>
2025-11-06 22:47:18 +0300

    Çemberin çevresi görevi tamamlandı(npm run test babel sürümü güncel olmadığı için hata vermişti ayrıca onu düzelttim).

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
+function CemberinCevresi(yaricap) {
   /* kodlar buraya */
+  return 2 * pi * yaricap;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
